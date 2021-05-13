4ce708b79860d45fab54398cf5bb242ab9b5c336
zhusq <18036161805@163.com>
2021-05-13 15:53:30 +0800

    api test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,55 +7,18 @@ namespace App\Http\Controllers;
 use App\Models\ZsqMoneyConsume;
 use Illuminate\Support\Facades\DB;
 
-class MoneyController extends Controller
+class TestController extends Controller
 {
     public function add()
     {
-        $params = $_POST;
-        $type = $params['type'];
-        $num = $params['num'];
-        $content = $params['content'];
         $data = [
-            'type' => $type,
-            'num' => $num,
-            'content' => $content,
-            'create_time' => date('Y-m-d H:i:s', time())
+            'name' => $_REQUEST['text'],
+            'num' => 222
         ];
-        $ret = DB::table('zsq_money')->insert($data);
-        if ($ret == 1) {
-            return json_encode([
-                'msg' => 'success',
-                'code' => 200,
-                'data' => $data
-            ]);
-        } else {
-            return json_encode([
-                'msg' => 'fail',
-                'code' => 5001,
-                'data' => $data
-            ]);
-        }
-
-    }
-
-    public function getCountMoney()
-    {
-        $total_money = ZsqMoneyConsume::getMonthTotal();
         return json_encode([
             'msg' => 'success',
             'code' => 200,
-            'total_money' => $total_money
-        ]);
-    }
-
-    public function getList()
-    {
-        $list = DB::table('zsq_money_consume')
-            ->get();
-        return json_encode([
-            'msg' => 'success',
-            'code' => 200,
-            'list' => $list
+            'data' => $data
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::any('test/add', 'TestController@add');
+//Route::middleware('cors')->group(function () {
+//
+//
+//
+//});
